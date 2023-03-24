82a749deb0cd796fecb2d10667a8c8fb64bb2320
4stro <100404622+4stro0@users.noreply.github.com>
2023-03-24 09:25:30 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+/* 
+ -- READ THIS --
+
+- You can change the server feature in index.html file and find the feature part / feature area
+- You can change your server about in index.html file and find the about part / about us area
+
+ -- CHANGE THE WEBSITE OPTION IN BELOW --
+*/
+let option = {
+    server_name: "HypePS",
+    server_description: "Best Growtopia Private Server",
+    owner: [
+        "a4stro#1119",
+        "My Name S#5562"
+    ],
+    discord_invite_link: "https://discord.gg/j2uYDuSNH3",
+    ip_address: "https://discord.com/channels/1080406529276117032/1082280019021271060",
+    host_for_android: "https://discord.com/channels/1080406529276117032/1082280019021271060"
+}
+
